9a945f4d69167dba5df9aa59f6865590952f9548
Horácio Macêdo <horacio97@gmail.com>
2022-05-01 03:12:13 -0300

    Spawner logic is brewing!
    It is also my first Coroutine on unity!

--- Assets/Scripts/GameOverseer.cs
+++ Assets/Scripts/GameOverseer.cs
@@ -8,15 +8,42 @@ public class GameOverseer : MonoBehaviour
 
     public PlayerManager Player;
     public List<EnemyTank> Enemies;
-    public List<SpawnerManager> Spawners;
+    public List<Spawner> Spawners;
+    public int maxConcurrentEnemies;
+    public bool waiting;
+
     // Start is called before the first frame update
     void Start()
     {
+        waiting = false;
         Player = GameObject.FindObjectOfType<PlayerManager>();
+    }
+
+    Spawner ElectSpawner(){
+        int spawnerIndex = Random.Range(0, 7);
+        if(!Spawners[spawnerIndex].isVisible) return Spawners[spawnerIndex];
+        else{
+            while(true){
+                if(!Spawners[spawnerIndex].isVisible) return Spawners[spawnerIndex];
+            }
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate() {
         Enemies = GameObject.FindObjectsOfType<EnemyTank>().ToList<EnemyTank>();
+        Spawners = GameObject.FindObjectsOfType<Spawner>().ToList<Spawner>();
+
+        if(Enemies.Count < maxConcurrentEnemies && waiting){
+            StartCoroutine(SpawnerActivation());
+        }
+    }
+
+    IEnumerator SpawnerActivation(){
+        Spawner elected = ElectSpawner();
+        elected.Spawn();
+        waiting = true;
+        yield return new WaitForSecondsRealtime(1);
+        waiting = false;
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnerManager : MonoBehaviour
+public class Spawner : MonoBehaviour
 {
+    public bool isVisible;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
+    }
+
+    public void Spawn() {
+
     }
 }
